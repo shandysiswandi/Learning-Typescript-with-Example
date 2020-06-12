1b5e6c406b9895288116dbb70a34cbb424981a9d
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 22:55:32 +0700

    add `any.ts`

---
+++ src/Basic Types/any.ts
@@ -0,0 +1,15 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+/**
+ * any : adalah tipe data dinamis yang bisa menampung apapun
+ *
+ * cara deklarasi sama seperti tipe data yang lain
+ * hanya isi dari varible yang ber tipe `any` ini bisa apa saja
+ */
+
+let boolean: any = true;
+let number: any = 1;
+let string: any = "Bisa saja";
+let array: any = [0, true, "bisa"];
