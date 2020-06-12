9f5f2cdc22c766ec564b43357b42bd5ea4a8a325
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 23:07:27 +0700

    add `null.ts`

---
+++ src/Basic Types/null.ts
@@ -0,0 +1,14 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+/**
+ * null : adalah variable yang bukan kosong dan juga bukan isi,
+ * dalam typescript variable ini mirip dengan undefined
+ */
+
+let a: null = null;
+const b: null = null;
+
+// jika dilihat sekilas, sangat tidak berguna, tapi jika dalam aplikasi yang dinamis
+// tipe data ini berguna loh.
