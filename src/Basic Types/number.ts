dd1b2986d1a3f60c53dfa3068a847772ec8ddd8f
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 21:47:15 +0700

    add `number.ts`

---
+++ src/Basic Types/number.ts
@@ -0,0 +1,12 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+// with let
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// with const
+const phi: number = 3.14;
