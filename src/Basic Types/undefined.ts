86df84987ad685272580281fa253cafe880d7b6d
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 23:09:48 +0700

    add `undefined.ts`

---
+++ src/Basic Types/undefined.ts
@@ -0,0 +1,14 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+/**
+ * undefined : adalah variable yang bukan kosong dan juga bukan isi,
+ * dalam typescript variable ini mirip dengan `null`
+ */
+
+let u: undefined = undefined;
+const v: undefined = undefined;
+
+// jika dilihat sekilas, sangat tidak berguna, tapi jika dalam aplikasi yang dinamis
+// tipe data ini berguna loh.
