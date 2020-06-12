837d252cb005392adc65b851a2a6ca7eae14d764
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 21:47:01 +0700

    add `boolean.ts`

---
+++ src/Basic Types/boolean.ts
@@ -0,0 +1,10 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+let isBook: boolean;
+isBook = false;
+// OR
+let isPerson: boolean = true;
+
+const isPhone: boolean = false;
