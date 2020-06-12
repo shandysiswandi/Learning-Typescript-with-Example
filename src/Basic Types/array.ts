3bae7b4be3f4e74ebe456ad880778da9536393ff
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 22:05:12 +0700

    add `array.ts`

---
+++ src/Basic Types/array.ts
@@ -0,0 +1,13 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+// cara 1
+let listBoolean: boolean[] = [true, false, true];
+let listNumber: number[] = [1, 2, 3];
+let listString: string[] = ["A", "B", "C"];
+
+// cara 2
+const listBoolean2: Array<boolean> = [true, false, true];
+const listNumber2: Array<number> = [1, 2, 3];
+const listString2: Array<string> = ["A", "B", "C"];
