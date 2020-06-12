9466f80f9c0f25be161a3ef6ef27452155403375
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 23:02:30 +0700

    add `void.ts`

---
+++ src/Basic Types/void.ts
@@ -0,0 +1,13 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+/**
+ * void : adalah sedikit seperti kebalikan dari `any`: tidak memiliki tipe apapun,
+ * kita biasanya dapat melihat `void` sebagai tipe pengembalian fungsi yang tidak mengembalikan nilai.
+ */
+
+// contoh 1
+function warnUser(): void {
+  console.log("This is my warning message");
+}
