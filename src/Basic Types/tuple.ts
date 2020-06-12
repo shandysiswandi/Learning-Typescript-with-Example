4f028415c9db42baca51ea7d18978b4c39415965
shandysiswandi <shandysiswandi@gmail.com>
2020-06-12 22:16:25 +0700

    add `tuple.ts`

---
+++ src/Basic Types/tuple.ts
@@ -0,0 +1,17 @@
+/*! *****************************************************************************
+Copyright (c) Learning Typescript with Example shandysiswandi. All rights reserved. MIT License.
+***************************************************************************** */
+
+/**
+ * Tuple : memungkinkan kita untuk mengekspresikan array dengan jumlah tetap elemen yang tipenya diketahui, tetapi tidak harus sama.
+ * isi dalam array bisa apa saja, seperti: number, boolean, string dan lain-lain
+ * */
+
+// deklarasi variable tuple type
+let x: [string, number];
+
+// inisialisasi variable
+x = ["hello", 10]; // cara yang benar
+
+// Initialize it incorrectly
+// x = [10, "hello"]; // cara yang salah, error
